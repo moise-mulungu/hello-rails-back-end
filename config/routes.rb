eb3c3e3a93bcd458340d5d48482331dbed76f9ac
moise <moisemlg90@gmail.com>
2022-08-09 21:48:28 +0200

    Setup the router file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :greetings
+  namespace :api, defaults: { format: 'json' } do
+    namespace :v1, defaults: { format: 'json' } do
+      get 'messages', to: 'greetings#index'
+    end
+  end
+
+      
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
